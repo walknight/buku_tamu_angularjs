5529f8a3eb83b5708a495f8dfaa38beba8949e64
walknight <darkwalkernight@gmail.com>
2015-09-02 15:25:09 +0700

    add loading indicator in app directive

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('CrudApp',['ngRoute','angular-loading-bar']);
+var app = angular.module('CrudApp',['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider){
     $routeProvider
@@ -15,29 +15,38 @@ app.config(['$routeProvider', function($routeProvider){
       
 }]);
 
-// app.directive('loading', function () {
-//     return {
-//       restrict: 'E',
-//       replace:true,
-//       template: '<div class="progress"><div class="indeterminate"></div></div>',
-//       link: function (scope, element, attr) {
-//               scope.$watch('loading', function (val) {
-//                 if (val)
-//                     $(element).show();
-//                 else
-//                     $(element).hide();
-//             });
-//         }
-//       }
-// })
+app.directive('loading', function () {
+    return {
+      restrict: 'E',
+      replace:true,
+      template: '<div class="progress"><div class="indeterminate"></div></div>',
+      link: function (scope, element, attr) {
+              scope.$watch('loading', function (val) {
+                if (val)
+                    $(element).show();
+                else
+                    $(element).hide();
+            });
+        }
+      };
+});
 
 function ListCtrl($scope, $http)
 {
-  // $scope.loading = true;
-  $http.get("http://ifcode.in/test_api.php?m=list").success(function(data){
-    //console.log(data);
-    $scope.guest=data.data;
-    // $scope.loading = false;
-  });
+  $scope.loading = true;
+  $http.get("http://ifcode.in/test_api.php?m=list")
+    .success(function(data){
+      //console.log(data);
+      $scope.guest=data.data;
+    
+    })
+    .finally(function(){
+      $scope.loading = false;
+    });
+  
+  $scope.clickMe = function(){
+    alert('test');
+  };
   
 }
+
